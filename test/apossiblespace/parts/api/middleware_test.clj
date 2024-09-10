5d77921c805b7e4aa6d3deb80e36496aa5e0d8b6
Gosha Tcherednitchenko <git@gosha.net>
2024-09-10 12:16:25 +0100

    logging: Add test for logging middleware

--- test/apossiblespace/parts/api/middleware_test.clj
+++ test/apossiblespace/parts/api/middleware_test.clj
@@ -1,6 +1,8 @@
 (ns apossiblespace.parts.api.middleware-test
   (:require [clojure.test :refer :all]
             [apossiblespace.parts.api.middleware :as middleware]
+            [com.brunobonacci.mulog :as mulog]
+            [com.brunobonacci.mulog.buffer :as rb]
             [ring.mock.request :as mock]
             [reitit.ring :as ring])
   (:import (org.sqlite SQLiteException SQLiteErrorCode)))
@@ -32,3 +34,26 @@
           response (app request)]
       (is (= 409 (:status response)))
       (is (= {:error "A resource with this unique identifier already exists"} (:body response))))))
+
+(deftest test-logging-middleware
+  (let [logged-entries (atom [])
+        handler (middleware/logging (fn [_request] {:status 200}))
+        request {:uri "/test" :request-method :get}]
+    (with-redefs [mulog/log (fn [event-type & data]
+                              (swap! logged-entries conj {:event-type event-type :data data}))]
+
+      (testing "generates logs from requests"
+        (handler request)
+        (let [{:keys [event-type data]} (last @logged-entries)]
+          (is (= :apossiblespace.parts.api.middleware/request event-type))
+          (is (= request (:request (apply hash-map data))))
+          (is (= false (:authenticated? (apply hash-map data))))))
+
+      (testing "logs user ID for authenticated requests"
+        (let [request (conj request {:identity {:sub "user-123"}})]
+          (handler request)
+          (let [{:keys [event-type data]} (last @logged-entries)]
+            (is (= :apossiblespace.parts.api.middleware/request event-type))
+            (is (= request (:request (apply hash-map data))))
+            (is (= true (:authenticated? (apply hash-map data))))
+            (is (= "user-123" (:user-id (apply hash-map data))))))))))
