229338328a9dab9b8e4fd628b3751cd9eb196ead
Gosha Tcherednitchenko <git@gosha.net>
2025-02-20 16:11:13 +0000

    feat(entities): system entity + tests + utils

---
+++ src/main/parts/entity/system.clj
@@ -0,0 +1,90 @@
+(ns parts.entity.system
+  "Entity representing a system map, including its nodes and edges.
+   Systems are owned by users and contain a collection of nodes and their
+   relationships (edges)."
+  (:require
+   [clojure.spec.alpha :as s]
+   [clojure.data.json :as json]
+   [parts.utils :refer [validate-spec]]
+   [parts.db :as db]))
+
+(s/def ::id string?)
+(s/def ::title string?)
+(s/def ::owner_id string?)
+(s/def ::viewport_settings (s/nilable string?))
+
+(s/def ::system (s/keys :req-un [::title ::owner_id]
+                        :opt-un [::id ::viewport_settings]))
+
+(defn create-system!
+  "Create a new system"
+  [data]
+  (validate-spec ::system data)
+  (db/insert! :systems data))
+
+(defn get-system
+  "Get a system by ID, including all its nodes and edges.
+
+   Returns a map containing:
+   - Basic system information (id, title, etc.)
+   - :nodes vector containing all nodes
+   - :edges vector containing all edges
+
+   Example:
+   ```
+   (get-system \"123\")
+   ;; => {:id \"123\"
+   ;;     :title \"My System\"
+   ;;     :owner_id \"user-456\"
+   ;;     :nodes [{:id \"n1\" :type \"manager\" ...}]
+   ;;     :edges [{:id \"e1\" :source_id \"n1\" ...}]}
+   ```"
+  [id]
+  (if-let [system (db/query-one
+                   (db/sql-format
+                    {:select [:*]
+                     :from [:systems]
+                     :where [:= :id id]}))]
+    (let [nodes (db/query
+                 (db/sql-format
+                  {:select [:*]
+                   :from [:nodes]
+                   :where [:= :system_id id]}))
+          edges (db/query
+                 (db/sql-format
+                  {:select [:*]
+                   :from [:edges]
+                   :where [:= :system_id id]}))]
+      (assoc system
+             :nodes nodes
+             :edges edges))
+    (throw (ex-info "System not found" {:type :not-found :id id}))))
+
+(defn list-systems
+  "List all systems for a user"
+  [owner-id]
+  (db/query
+   (db/sql-format
+    {:select [:*]
+     :from [:systems]
+     :where [:= :owner_id owner-id]})))
+
+
+(defn update-system!
+  "Update a system"
+  [id data]
+  (validate-spec ::system (assoc data :id id))
+  (let [updated (db/update! :systems
+                            (assoc data :last_modified [:raw "CURRENT_TIMESTAMP"])
+                            [:= :id id])]
+    (if (seq updated)
+      (first updated)
+      (throw (ex-info "System not found" {:type :not-found :id id})))))
+
+(defn delete-system!
+  "Delete a system. Returns a map with :id and :deleted keys,
+   where :deleted is true if the system was found and deleted."
+  [id]
+  (let [result (db/delete! :systems [:= :id id])
+        count (or (:next.jdbc/update-count (first result)) 0)]
+    {:id id :deleted (pos? count)}))
---
+++ test/parts/entity/system_test.clj
@@ -0,0 +1,60 @@
+(ns parts.entity.system-test
+  (:require
+   [clojure.test :refer [deftest is testing use-fixtures]]
+   [parts.entity.system :as system]
+   [parts.helpers.utils :refer [with-test-db register-test-user]]))
+
+(use-fixtures :once with-test-db)
+
+(deftest test-system-crud
+  (let [user (register-test-user)
+        system-data {:title "Test System"
+                    :owner_id (:id user)}]
+
+    (testing "create-system!"
+      (let [created (system/create-system! system-data)]
+        (is (string? (:id created)))
+        (is (= (:title system-data) (:title created)))
+        (is (= (:owner_id system-data) (:owner_id created)))
+        (is (some? (:created_at created)))
+        (is (some? (:last_modified created)))))
+
+    (testing "get-system"
+      (let [created (system/create-system! system-data)
+            fetched (system/get-system (:id created))]
+        (is (= (:id created) (:id fetched)))
+        (is (= (:title created) (:title fetched)))
+        (is (vector? (:nodes fetched)))
+        (is (vector? (:edges fetched)))))
+
+    (testing "list-systems"
+      (let [_ (system/create-system! system-data)
+            systems (system/list-systems (:id user))]
+        (is (seq systems))
+        (is (every? #(= (:owner_id %) (:id user)) systems))))
+
+    (testing "update-system!"
+      (let [created (system/create-system! system-data)
+            updated (system/update-system! (:id created) {:title "Updated Title"
+                                                        :owner_id (:id user)})]
+        (is (= "Updated Title" (:title updated)))
+        (is (= (:id created) (:id updated)))))
+
+    (testing "delete-system!"
+      (let [created (system/create-system! system-data)
+            result (system/delete-system! (:id created))]
+        (is (:deleted result))
+        (is (= (:id created) (:id result)))
+        (is (thrown-with-msg? clojure.lang.ExceptionInfo #"System not found"
+                              (system/get-system (:id created))))))))
+
+(deftest test-system-validations
+  (testing "creates fails with invalid data"
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Validation failed"
+                         (system/create-system! {}))))
+
+  (testing "update fails with invalid data"
+    (let [user (register-test-user)
+          system (system/create-system! {:title "Test" :owner_id (:id user)})]
+      (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Validation failed"
+                           (system/update-system! (:id system) {:title nil}))))))
