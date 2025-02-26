53e48e14bf052ccf4a94ede3d06dc4a70e171c37
Gosha Tcherednitchenko <git@gosha.net>
2025-02-26 15:52:26 +0000

    feat(analytics): add event tracking

--- src/main/parts/frontend/app.cljs
+++ src/main/parts/frontend/app.cljs
@@ -18,14 +18,50 @@
   ($ system system-data))
 
 (defonce root
-  (uix.dom/create-root (js/document.getElementById "root")))
+  (when-let [root-element (js/document.getElementById "root")]
+    (uix.dom/create-root root-element)))
+
+(defn track-source-attribution []
+  (when (and js/document.referrer js/window.plausible)
+    (let [params (js/URLSearchParams. js/window.location.search)]
+      (js/window.plausible
+       "Source"
+       #js{:props #js{:referrer js/document.referrer
+                      :campaign (.get params "utm_campaign")
+                      :source (.get params "utm_source")
+                      :medium (.get params "utm_medium")}}))))
+
+(defn setup-form-tracking []
+  (when js/window.plausible
+    (let [emailField (js/document.getElementById "email")]
+      (when emailField
+        (.addEventListener
+         emailField "focus"
+         #(js/window.plausible
+           "EmailFocus"
+           #js{:props #js{:field "email"}}))))
+
+    (let [form (js/document.querySelector "#signup-form form")]
+      (when form
+        (.addEventListener
+         form "submit"
+         #(js/window.plausible
+           "FormSubmission"
+           #js{:props #js{:form "waitlist-signup"}}))))))
+
+(defn render-app []
+  "Render the app if root element exists"
+  (when root
+    (uix.dom/render-root ($ app) root)))
 
 (defn ^:export init []
   (.on htmx "htmx:load"
        (fn [_]
-         (uix.dom/render-root ($ app) root)
+         (render-app)
          (let [version (.-version htmx)]
-           (js/console.log "HTMX loaded! Version:" version)))))
+           (js/console.log "HTMX loaded! Version:" version))
+         (track-source-attribution)
+         (setup-form-tracking))))
 
 (defn ^:dev/after-load reload! []
   (js/console.log "Reloading app...")
