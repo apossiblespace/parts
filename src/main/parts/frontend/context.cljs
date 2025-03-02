437b3b07d933906f606f557fa3259303b303c5fb
Gosha Tcherednitchenko <git@gosha.net>
2025-03-02 18:55:37 +0000

    feat(frontend): add auth context

--- src/main/parts/frontend/context.cljs
+++ src/main/parts/frontend/context.cljs
@@ -2,3 +2,9 @@
   (:require [uix.core]))
 
 (def update-node-context (uix.core/create-context nil))
+
+(def auth-context (uix.core/create-context 
+                    {:logged-in false
+                     :email nil
+                     :login nil
+                     :logout nil}))
