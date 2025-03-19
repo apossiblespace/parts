e17710183d48c27722ee494672988a0b896ec2fe
Gosha Tcherednitchenko <git@gosha.net>
2025-03-19 13:18:08 +0000

    chore: small refactor of node component

--- src/main/parts/frontend/components/nodes.cljs
+++ src/main/parts/frontend/components/nodes.cljs
@@ -1,29 +1,27 @@
 (ns parts.frontend.components.nodes
   (:require
    ["reactflow" :refer [Handle Position]]
-   [uix.core :refer [defui $ use-state]]
-   [parts.frontend.components.node-form :refer [node-form]]
-   [parts.common.constants :refer [part-labels]]
-   [parts.frontend.context :as ctx]))
+   [uix.core :refer [defui $]]
+   [parts.common.constants :refer [part-labels]]))
 
-(defui parts-node [{:keys [id type data is-connectable]}]
+(defui parts-node [{:keys [type data is-connectable]}]
   ($ :div {:class "node-wrapper"}
      ($ :div {:class (str "node " type)}
         ($ Handle {:type "target"
                    :position (.-Top Position)
                    :isConnectable is-connectable})
-        ($ :div
-           (get data "label")
-           ($ Handle {:type "source"
-                      :position (.-Bottom Position)
-                      :isConnectable is-connectable})))))
+        ($ :div {:class "text-center font-medium text-sm/4"}
+           (:label data))
+        ($ Handle {:type "source"
+                   :position (.-Bottom Position)
+                   :isConnectable is-connectable}))))
 
 (def PartsNode
   (uix.core/as-react
    (fn [{:keys [id type data isConnectable] :as ^js props}]
      ($ parts-node {:id id
                     :type type
-                    :data (js->clj data)
+                    :data (js->clj data :keywordize-keys true)
                     :is-connectable isConnectable}))))
 
 (def node-types
