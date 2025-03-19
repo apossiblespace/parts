2d505d813fdb3ee3e72a240d41af5484124e94b0
Gosha Tcherednitchenko <git@gosha.net>
2025-03-19 13:18:28 +0000

    feat(sidebar): handle saving on enter key

--- src/main/parts/frontend/components/node_form.cljs
+++ src/main/parts/frontend/components/node_form.cljs
@@ -5,13 +5,25 @@
 
 (defui node-form [{:keys [node on-save]}]
   (let [{:keys [id type data]} node
-        [form-data set-form-data] (uix.core/use-state {:type type
-                                                       :label (:label data)})]
-    ($ :fieldset {:class "fieldset node-form p-2 border-b border-b-1 border-base-300"}
+        [initial-form-data set-initial-form-data] (uix.core/use-state {:type type, :label (:label data)})
+        [form-data set-form-data] (uix.core/use-state initial-form-data)
+        changed? (not= form-data initial-form-data)
+        handle-save (fn []
+                      (on-save id form-data)
+                      (set-initial-form-data form-data)
+                      (when (.-activeElement js/document)
+                        (.blur (.-activeElement js/document))))
+        handle-submit (fn [e]
+                        (.preventDefault e)
+                        (when changed?
+                          (handle-save)))]
+    ($ :form {:onSubmit handle-submit
+              :class "fieldset node-form p-2 border-b border-b-1 border-base-300"}
        ($ :div {:class "flex justify-between"}
           ($ :h3 {:class "text-xs/4 mr-2 font-bold"} (:label form-data))
           ($ :button {:class "btn btn-primary btn-xs"
-                      :onClick #(on-save id form-data)}
+                      :disabled (not changed?)
+                      :type "submit"}
              "Save"))
 
        ($ :label {:class "fieldset-label"} "Type:")
