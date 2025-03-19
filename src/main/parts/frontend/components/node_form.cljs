f25081be5360ff2f28a4001a02a23a53d02b7056
Gosha Tcherednitchenko <git@gosha.net>
2025-03-19 06:36:25 +0000

    feat(sidebar): use fieldset in node form

--- src/main/parts/frontend/components/node_form.cljs
+++ src/main/parts/frontend/components/node_form.cljs
@@ -3,25 +3,34 @@
    [uix.core :refer [defui $]]
    [parts.common.constants :refer [part-labels]]))
 
+;; TODO: Remove the node-form from the actual node. Editing will happen in the
+;; sidebar henceforth.
 (defui node-form [{:keys [node on-save on-cancel]}]
   (let [{:keys [id type data]} node
         [form-data set-form-data] (uix.core/use-state {:type type
-                                                       :label (get data "label")})]
-    ($ :div {:class "node-form"}
-       ($ :div {:class "form-row"}
-          ($ :label "Type:")
-          ($ :select {:value (:type form-data)
-                      :onChange #(set-form-data assoc :type (.. % -target -value))}
-             (->> part-labels
-                  (map (fn [[k {:keys [label]}]]
-                         ($ :option {:key k :value k}
-                            label))))))
-       ($ :div {:class "form-row"}
-          ($ :label "Label:")
-          ($ :input {:type "text"
-                     :value (:label form-data)
-                     :onChange #(set-form-data assoc :label (.. % -target -value))}))
+                                                       :label (:label data)})]
+    ($ :fieldset {:class "fieldset node-form"}
+       ($ :legend {:class "fieldset-legend"}
+          (str "#" id ": " (:label form-data)))
 
-       ($ :div {:class "form-actions"}
-          ($ :button {:onClick #(on-cancel)} "Cancel")
-          ($ :button {:onClick #(on-save id form-data)} "Save")))))
+       ($ :label {:class "fieldset-label"} "Type:")
+       ($ :select {:class "select"
+                   :value (:type form-data)
+                   :onChange #(set-form-data assoc :type (.. % -target -value))}
+          (->> part-labels
+               (map (fn [[k {:keys [label]}]]
+                      ($ :option {:key k :value k}
+                         label)))))
+
+       ($ :label {:class "fieldset-label"} "Label:")
+       ($ :input {:class "input"
+                  :type "text"
+                  :value (:label form-data)
+                  :onChange #(set-form-data assoc :label (.. % -target -value))})
+
+       ($ :button {:class "btn"
+                   :onClick #(on-cancel)}
+          "Cancel")
+       ($ :button {:class "btn btn-primary"
+                   :onClick #(on-save id form-data)}
+          "Save"))))
