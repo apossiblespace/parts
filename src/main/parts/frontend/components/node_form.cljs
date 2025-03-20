64ba13eeaa4a68e98a4e2c35fac2d226efa88b6a
Gosha Tcherednitchenko <git@gosha.net>
2025-03-20 11:23:02 +0000

    feat(sidebar): collapse when multiple selected

--- src/main/parts/frontend/components/node_form.cljs
+++ src/main/parts/frontend/components/node_form.cljs
@@ -1,12 +1,14 @@
 (ns parts.frontend.components.node-form
   (:require
-   [uix.core :refer [defui $]]
-   [parts.common.constants :refer [part-labels]]))
+   [parts.common.constants :refer [part-labels]]
+   [uix.core :refer [$ defui]]))
 
-(defui node-form [{:keys [node on-save]}]
+(defui node-form [{:keys [node on-save collapsed]}]
+  (println "collapsed" collapsed)
   (let [{:keys [id type data]} node
         [initial-form-data set-initial-form-data] (uix.core/use-state {:type type, :label (:label data)})
         [form-data set-form-data] (uix.core/use-state initial-form-data)
+        [collapsed? set-collapsed] (uix.core/use-state collapsed)
         changed? (not= form-data initial-form-data)
         handle-save (fn []
                       (on-save id form-data)
@@ -20,25 +22,48 @@
     ($ :form {:onSubmit handle-submit
               :class "fieldset node-form p-2 border-b border-b-1 border-base-300"}
        ($ :div {:class "flex justify-between"}
-          ($ :h3 {:class "text-xs/4 mr-2 font-bold"} (:label form-data))
-          ($ :button {:class "btn btn-primary btn-xs"
-                      :disabled (not changed?)
-                      :type "submit"}
-             "Save"))
+          ($ :h3 {:class "text-xs/4 mr-2 font-bold cursor-pointer w-full pl-3 relative"
+                  :on-click #(set-collapsed (not collapsed?))}
+             (if collapsed?
+               ($ :svg
+                  {:xmlns "http://www.w3.org/2000/svg"
+                   :viewBox "0 0 16 16"
+                   :fill "currentColor"
+                   :class "size-4 absolute -left-1"}
+                  ($ :path
+                     {:fill-rule "evenodd"
+                      :d "M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z"
+                      :clip-rule "evenodd"}))
+               ($ :svg
+                  {:xmlns "http://www.w3.org/2000/svg"
+                   :viewBox "0 0 16 16"
+                   :fill "currentColor"
+                   :class "size-4 absolute -left-1"}
+                  ($ :path
+                     {:fill-rule "evenodd"
+                      :d "M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                      :clip-rule "evenodd"})))
+             ($ :span (:label form-data)))
+          (when-not collapsed?
+            ($ :button {:class "btn btn-primary btn-xs"
+                        :disabled (not changed?)
+                        :type "submit"}
+               "Save")))
+       (when-not collapsed?
+         ($ :div
+            ($ :label {:class "fieldset-label"} "Type:")
+            ($ :select {:class "select select-sm mb-1"
+                        :value (:type form-data)
+                        :onChange #(set-form-data assoc :type (.. % -target -value))}
+               (->> part-labels
+                    (map (fn [[k {:keys [label]}]]
+                           ($ :option {:key k :value k}
+                              label)))))
 
-       ($ :label {:class "fieldset-label"} "Type:")
-       ($ :select {:class "select"
-                   :value (:type form-data)
-                   :onChange #(set-form-data assoc :type (.. % -target -value))}
-          (->> part-labels
-               (map (fn [[k {:keys [label]}]]
-                      ($ :option {:key k :value k}
-                         label)))))
+            ($ :label {:class "fieldset-label"} "Label:")
+            ($ :input {:class "input input-sm mb-1"
+                       :type "text"
+                       :value (:label form-data)
+                       :onChange #(set-form-data assoc :label (.. % -target -value))})
 
-       ($ :label {:class "fieldset-label"} "Label:")
-       ($ :input {:class "input"
-                  :type "text"
-                  :value (:label form-data)
-                  :onChange #(set-form-data assoc :label (.. % -target -value))})
-
-       ($ :p {:class "fieldset-label"} (str "id: " id)))))
+            ($ :p {:class "fieldset-label"} (str "id: " id)))))))
--- src/main/parts/frontend/components/sidebar.cljs
+++ src/main/parts/frontend/components/sidebar.cljs
@@ -10,20 +10,26 @@
   [{:keys [selected-nodes selected-edges]}]
   (println "Sidebar selected nodes:" selected-nodes)
   (println "Sidebar selected edges:" selected-edges)
-  (let [update-node (uix.core/use-context ctx/update-node-context)]
+  (let [update-node (uix.core/use-context ctx/update-node-context)
+        node-count (count selected-nodes)
+        multiple-nodes (> node-count 1)]
+    (println "Node count:" node-count)
     ($ :div {:class "sidebar"}
        ($ auth-status)
+       (when multiple-nodes
+         ($ :div {:class "text-xs p-2"} (str node-count " parts selected:")))
        (when (seq selected-nodes)
          ($ :section {:class "selected-nodes"}
             (map
-             (fn [node]
-               ($ node-form {:key (:id node)
-                             :node node
-                             :on-save (fn [id form-data]
-                                        (println "on-save called" id form-data)
-                                        (when update-node
-                                          (update-node id form-data)))}))
-             selected-nodes)))
+              (fn [node]
+                ($ node-form {:key (str (:id node) node-count)
+                              :node node
+                              :collapsed multiple-nodes
+                              :on-save (fn [id form-data]
+                                         (println "on-save called" id form-data)
+                                         (when update-node
+                                           (update-node id form-data)))}))
+              selected-nodes)))
        (when (seq selected-edges)
          ($ :div {:class "selected-edges"}
             "Selected Edges")))))
