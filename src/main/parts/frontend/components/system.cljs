346f76fce69f5d2bb657b13075ffffcc5453a0db
Gosha Tcherednitchenko <git@gosha.net>
2025-03-22 06:17:40 +0000

    feat(system): simplify hooks + format

--- src/main/parts/frontend/components/system.cljs
+++ src/main/parts/frontend/components/system.cljs
@@ -1,29 +1,22 @@
 (ns parts.frontend.components.system
   (:require
-   ["reactflow" :refer [ReactFlow
-                        MiniMap
-                        Controls
-                        Background
-                        useNodesState
-                        useEdgesState
-                        Panel
-                        addEdge]]
-   [uix.core :refer [defui $ use-state]]
+   ["reactflow" :refer [Background Controls MiniMap Panel ReactFlow addEdge useEdgesState useNodesState]]
    [clojure.string :as str]
    [parts.frontend.components.nodes :refer [node-types]]
-   [parts.frontend.components.toolbar :refer [parts-toolbar]]
    [parts.frontend.components.sidebar :refer [sidebar]]
+   [parts.frontend.components.toolbar :refer [parts-toolbar]]
+   [parts.frontend.context :as ctx]
    [parts.frontend.utils.node-utils :refer [build-updated-part]]
-   [parts.frontend.context :as ctx]))
+   [uix.core :refer [$ defui use-callback use-memo use-state]]))
 
 ;; FIXME: This shouldn't be returning a javascript object.
 ;; Ideally, we would not be manipulating JS outside of the React component at
 ;; all -- it should all be Clojure.
 (defn- new-node [type _opts]
-  #js{:id (str (random-uuid))
-      :type type
-      :position #js{:x 390 :y 290}
-      :data #js{:label (str/capitalize type)}})
+  #js {:id (str (random-uuid))
+       :type type
+       :position #js {:x 390 :y 290}
+       :data #js {:label (str/capitalize type)}})
 
 (defn- add-node
   ([type]
@@ -31,7 +24,7 @@
   ([type opts]
    (fn [current-nodes]
      (.concat current-nodes
-              #js[(new-node type opts)]))))
+              #js [(new-node type opts)]))))
 
 ;; NOTE: Layouting
 ;; https://reactflow.dev/learn/layouting/layouting
@@ -41,37 +34,37 @@
 (defn- update-node-callback [setNodes id form-data]
   (println "update-node-callback" id form-data)
   (setNodes
-   (fn [nodes]
-     (clj->js
-      (map (fn [node]
-             (let [node-map (js->clj node :keywordize-keys true)]
-               (if (= (:id node-map) id)
-                 (build-updated-part node-map form-data)
-                 node-map)))
-           (js->clj nodes :keywordize-keys true))))))
+    (fn [nodes]
+      (clj->js
+        (map (fn [node]
+               (let [node-map (js->clj node :keywordize-keys true)]
+                 (if (= (:id node-map) id)
+                   (build-updated-part node-map form-data)
+                   node-map)))
+             (js->clj nodes :keywordize-keys true))))))
 
 (defn- on-connect-callback [setEdges params]
   (setEdges #(addEdge params %)))
 
 (defui system [{:keys [nodes edges]}]
-  (let [node-types (uix.core/use-memo (fn [] (clj->js node-types)) [node-types])
+  (let [node-types (use-memo (fn [] (clj->js node-types)) [node-types])
         [nodes setNodes onNodesChange] (useNodesState (clj->js nodes))
         [edges setEdges onEdgesChange] (useEdgesState (clj->js edges))
-        [selected-nodes set-selected-nodes] (uix.core/use-state nil)
-        [selected-edges set-selected-edges] (uix.core/use-state nil)
-        update-node (uix.core/use-callback
-                     #(update-node-callback setNodes %1 %2)
-                     [setNodes])
-        on-connect (uix.core/use-callback
-                    #(on-connect-callback setEdges %)
-                    [setEdges])
-        on-selection-change (uix.core/use-callback
-                             (fn [selections]
-                               (let [sel (js->clj selections :keywordize-keys true)]
-                                 (println "Sel:" sel)
-                                 (set-selected-nodes (:nodes sel))
-                                 (set-selected-edges (:edges sel))))
-                             [])]
+        [selected-nodes set-selected-nodes] (use-state nil)
+        [selected-edges set-selected-edges] (use-state nil)
+        update-node (use-callback
+                      #(update-node-callback setNodes %1 %2)
+                      [setNodes])
+        on-connect (use-callback
+                     #(on-connect-callback setEdges %)
+                     [setEdges])
+        on-selection-change (use-callback
+                              (fn [selections]
+                                (let [sel (js->clj selections :keywordize-keys true)]
+                                  (println "Sel:" sel)
+                                  (set-selected-nodes (:nodes sel))
+                                  (set-selected-edges (:edges sel))))
+                              [])]
 
     ($ (.-Provider ctx/update-node-context) {:value update-node}
        ($ :div {:class "system-container"}
