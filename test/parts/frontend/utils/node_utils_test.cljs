1d47b7c7b41d69333e7a50d9217e6cb01af5406a
Gosha Tcherednitchenko <git@gosha.net>
2025-03-30 14:36:57 +0100

    fix: failed tests from previous commit

--- test/parts/frontend/utils/node_utils_test.cljs
+++ test/parts/frontend/utils/node_utils_test.cljs
@@ -4,28 +4,28 @@
 
 (deftest build-updated-part-test
   (testing "when changing type with default label"
-    (let [node {:type "manager"
-                :data {:label "Manager"}}
+    (let [node {:data {:type "manager" :label "Manager"}}
           form {:type "unknown"
                 :label "Manager"}
           result (nu/build-updated-part node form)]
-      (is (= "unknown" (:type result)))
-      (is (= {"label" "Unknown"} (:data result)))))
+      (is (= {:data {:type "unknown"
+                     :label "Manager"}}
+             result))))
 
   (testing "when changing type with custom label"
-    (let [node {:type "manager"
-                :data {:label "My Custom Manager"}}
+    (let [node {:data {:type "manager" :label "My Custom Manager"}}
           form {:type "unknown"
                 :label "My Custom Manager"}
           result (nu/build-updated-part node form)]
-      (is (= "unknown" (:type result)))
-      (is (= {"label" "My Custom Manager"} (:data result)))))
+      (is (= {:data {:type "unknown"
+                     :label "My Custom Manager"}}
+             result))))
 
   (testing "when changing type and explicitly changing label"
-    (let [node {:type "manager"
-                :data {:label "Manager"}}
+    (let [node {:data {:type "manager" :label "Manager"}}
           form {:type "unknown"
                 :label "Custom Unknown"}
           result (nu/build-updated-part node form)]
-      (is (= "unknown" (:type result)))
-      (is (= {"label" "Custom Unknown"} (:data result))))))
+      (is (= {:data {:type "unknown"
+                     :label "Custom Unknown"}}
+             result)))))
